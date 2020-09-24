4e0c3ba9aae2a456443830fdb16e06eabd26c54d
goffjira <goffjira@gmail.com>
2020-09-24 17:37:55 +0700

    Make compoment showing movie  poster able to render  both vertical&horizonal

--- src/App.js
+++ src/App.js
@@ -5,12 +5,22 @@ import requests from "./requests";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <Navbar />
+      <Banner />
+
       <Row
         title="Netflix Originals"
         fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
       />
       <Row title="Trendings Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentary" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from "react";
 import "./Row.css";
 import axios from "./axios";
 
-const base_url = "https://image.tmdb.org/t/p/original/";
+const base_url = "https://image.tmdb.org/t/p/w300";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow = false }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -21,8 +21,10 @@ function Row({ title, fetchUrl }) {
       <div className="row__posters">
         {movies.map((movie) => (
           <img
-            className="row__poster"
-            src={`${base_url}${movie.poster_path}`}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.original_name}
           />
         ))}
