ad3da42b82198b0c275aa2bc582d0a818221e16b
goffjira <goffjira@gmail.com>
2020-09-28 19:14:37 +0700

    Add RowList component to serve as Rows container

---
+++ src/RowList.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Row from "./Row";
+import "./RowList.css";
+import requests from "./requests";
+
+function RowList() {
+  return (
+    <div class="rowList">
+      <Row
+        title="Netflix Originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="Trendings Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentary" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
+}
+
+export default RowList;
