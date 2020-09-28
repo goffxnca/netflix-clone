9b44f12659eaf7ec56feb4e7f8820ebf89959626
goffjira <goffjira@gmail.com>
2020-09-28 14:33:41 +0700

    Encapsulate Poster style and behavior to it own component

---
+++ src/Poster.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import movieTrailer from "movie-trailer";
+import "./Poster.css";
+import Action from "./Action";
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import AddIcon from "@material-ui/icons/Add";
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import ThumbDownIcon from "@material-ui/icons/ThumbDown";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+const base_url = "https://image.tmdb.org/t/p/w500";
+
+function Poster({ movie, isLargeRow }) {
+  const [youtubeTrailerId, setYoutubeTrailerId] = useState("");
+
+  const handleClickPoster = (movie) => {
+    if (youtubeTrailerId) {
+      setYoutubeTrailerId("");
+    } else {
+      //Find movie trailer from youtube
+      const movieName = movie?.name || movie?.orinal_title || movie?.title;
+      movieTrailer(movieName)
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          const trailerId = urlParams.get("v");
+          setYoutubeTrailerId(trailerId);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // autoplay: 1,
+    },
+  };
+
+  return (
+    <div className="poster">
+      <img
+        className="poster__img"
+        src={`${base_url}${
+          isLargeRow ? movie.poster_path : movie.backdrop_path
+        }`}
+        alt={movie.original_name}
+        onClick={() => handleClickPoster(movie)}
+      />
+      <div className="poster__content">
+        <div className="poster__actions">
+          <Action
+            Icon={PlayArrowIcon}
+            solidStyle={true}
+            tooltip="Add to My List"
+          />
+          <Action Icon={AddIcon} tooltip="Add to My List" />
+          <Action Icon={ThumbUpAltIcon} tooltip="" />
+          <Action Icon={ThumbDownIcon} />
+          <Action Icon={ExpandMoreIcon} />
+        </div>
+        <div className="poster__info">Info</div>
+      </div>
+    </div>
+  );
+}
+
+export default Poster;
--- src/Row.js
+++ src/Row.js
@@ -1,14 +1,10 @@
 import React, { useEffect, useState } from "react";
 import "./Row.css";
 import axios from "./axios";
-import Youtube from "react-youtube";
-import movieTrailer from "movie-trailer";
-
-const base_url = "https://image.tmdb.org/t/p/w300";
+import Poster from "./Poster";
 
 function Row({ title, fetchUrl, isLargeRow = false }) {
   const [movies, setMovies] = useState([]);
-  const [youtubeTrailerId, setYoutubeTrailerId] = useState("");
 
   useEffect(() => {
     axios.get(fetchUrl).then((resp) => {
@@ -16,50 +12,15 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
     });
   }, [fetchUrl]);
 
-  const opts = {
-    height: "390",
-    width: "100%",
-    playerVars: {
-      // autoplay: 1,
-    },
-  };
-
-  const handleClickPoster = (movie) => {
-    if (youtubeTrailerId) {
-      setYoutubeTrailerId("");
-    } else {
-      //Find movie trailer from youtube
-      const movieName = movie?.name || movie?.orinal_title || movie?.title;
-      movieTrailer(movieName)
-        .then((url) => {
-          const urlParams = new URLSearchParams(new URL(url).search);
-          const trailerId = urlParams.get("v");
-          setYoutubeTrailerId(trailerId);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
-  };
-
   return (
     <div className="row">
       <h2>{title}</h2>
 
       <div className="row__posters">
-        {movies.map((movie) => (
-          <img
-            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-            src={`${base_url}${
-              isLargeRow ? movie.poster_path : movie.backdrop_path
-            }`}
-            alt={movie.original_name}
-            onClick={() => handleClickPoster(movie)}
-          />
+        {movies.slice(0, 6).map((movie) => (
+          <Poster movie={movie} isLargeRow={isLargeRow} />
         ))}
       </div>
-
-      {youtubeTrailerId && <Youtube videoId={youtubeTrailerId} opts={opts} />}
     </div>
   );
 }
