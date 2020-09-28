3a591d887d13c4bfa3f55aaf9c4e4085cafb70e9
goffjira <goffjira@gmail.com>
2020-09-28 19:17:24 +0700

    Add CSS rule to support seemlessly scaledown Banner,Navbar,Post

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React from "react";
 import "./App.css";
-import Row from "./Row";
-import requests from "./requests";
 import Banner from "./Banner";
 import Navbar from "./Navbar";
+import RowList from "./RowList";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
       <Banner />
-      <Row
-        title="Netflix Originals"
-        fetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow={true}
-      />
-      <Row title="Trendings Now" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-      <Row title="Action" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Comedy" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Horror" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="Romance" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Documentary" fetchUrl={requests.fetchDocumentaries} />
+      <RowList />
     </div>
   );
 }
