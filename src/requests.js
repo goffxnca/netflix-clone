683447ed4d3c4c8795e4e6218a73ea614c93982a
goffjira <goffjira@gmail.com>
2020-09-29 14:48:05 +0700

    Show prev&next array button when hovering on Row

--- src/requests.js
+++ src/requests.js
@@ -2,7 +2,7 @@ const APIKey = "61cc89b61fe69dd43640b41d9ecf852a";
 
 const requests = {
   fetchTrending: `/trending/all/day?api_key=${APIKey}`,
-  fetchNetflixOriginals: `/discover/tv/?width_networks=231&api_key=${APIKey}`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${APIKey}`,
   fetchTopRated: `/movie/top_rated?language=en-US&api_key=${APIKey}`,
   fetchActionMovies: `/discover/movie?with_genres=28&api_key=${APIKey}`,
   fetchComedyMovies: `/discover/movie?with_genres=35&api_key=${APIKey}`,
